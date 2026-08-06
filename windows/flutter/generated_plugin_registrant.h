1a8515086ce09e646b9376baf83f921737fde286
KlTlK <kirillmuhortov43@gmail.com>
2026-08-06 15:46:52 +0300

    add windows/flutter/generated_plugin_registrant.h

---
+++ windows/flutter/generated_plugin_registrant.h
@@ -0,0 +1,6 @@
+// Generated - do not edit.
+#ifndef GENERATED_PLUGIN_REGISTRANT_
+#define GENERATED_PLUGIN_REGISTRANT_
+#include <flutter/plugin_registry.h>
+void RegisterPlugins(flutter::PluginRegistry* registry);
+#endif
