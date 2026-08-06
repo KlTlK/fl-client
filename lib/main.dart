f5e07cd827a9e98e1af0cd4036df6094effa359c
KlTlK <kirillmuhortov43@gmail.com>
2026-08-06 14:37:05 +0300

    add lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'theme/app_theme.dart';
+import 'state/vpn_state.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => VpnState(),
+      child: const FlClientApp(),
+    ),
+  );
+}
+
+class FlClientApp extends StatelessWidget {
+  const FlClientApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'fl-client',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.dark,
+      home: const HomeScreen(),
+    );
+  }
+}
