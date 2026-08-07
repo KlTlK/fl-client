45c9e658ef45d3db72cf28e7e2d045dd4bdaf1a1
KlTlK <kirillmuhortov43@gmail.com>
2026-08-07 08:33:22 +0300

    fix windows/runner/main.cpp

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,6 +1,7 @@
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
+
 #include "flutter_window.h"
 #include "utils.h"
 
@@ -9,20 +10,28 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
+
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+
   flutter::DartProject project(L"data");
+
   std::vector<std::string> command_line_arguments = GetCommandLineArguments();
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
+
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(420, 760);
-  if (!window.CreateAndShow(L"fl-client", origin, size)) return EXIT_FAILURE;
+  if (!window.Create(L"fl-client", origin, size)) {
+    return EXIT_FAILURE;
+  }
   window.SetQuitOnClose(true);
+
   ::MSG msg;
   while (::GetMessage(&msg, nullptr, 0, 0)) {
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
   }
+
   ::CoUninitialize();
   return EXIT_SUCCESS;
 }
