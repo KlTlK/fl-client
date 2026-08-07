8a9a13ffefe991b55afd6af63948fab76d01a332
KlTlK <kirillmuhortov43@gmail.com>
2026-08-07 20:57:54 +0300

    fix lib/theme/app_theme.dart

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const bg = Color(0xFF1A1B2E);
-  static const surface = Color(0xFF252640);
-  static const card = Color(0xFF2D2E4A);
-  static const accent = Color(0xFF7C8CF8);
-  static const accentSoft = Color(0xFFA5B4FC);
-  static const success = Color(0xFF6EE7B7);
-  static const danger = Color(0xFFFCA5A5);
-  static const textPrimary = Color(0xFFE2E8F0);
-  static const textSecondary = Color(0xFF94A3B8);
+  static const bg = Color(0xFF0D0B1A);
+  static const surface = Color(0xFF1A1530);
+  static const card = Color(0xFF241D3D);
+  static const accent = Color(0xFF6C7BF8);
+  static const accentSoft = Color(0xFF8B9CF7);
+  static const success = Color(0xFF5EEAD4);
+  static const danger = Color(0xFFF87171);
+  static const textPrimary = Color(0xFFE8E4F0);
+  static const textSecondary = Color(0xFF8B82A8);
 
   static ThemeData get dark => ThemeData(
     useMaterial3: true,
