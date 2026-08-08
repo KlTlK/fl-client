1be05090209c5afd75dd8ef74745c7066e8e8428
KlTlK <kirillmuhortov43@gmail.com>
2026-08-08 09:43:06 +0300

    fix windows/runner/main.cpp

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -7,6 +7,9 @@
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
+  // Enable high DPI awareness BEFORE any window creation
+  ::SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
